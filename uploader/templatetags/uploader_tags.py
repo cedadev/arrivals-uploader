fd813319f13594f17043d12beb70d5d388d4ffd4
watucker <william.tucker@stfc.ac.uk>
2019-12-05 15:52:02 +0000

    Fixed bug when getting size of symlink

--- uploader/templatetags/uploader_tags.py
+++ uploader/templatetags/uploader_tags.py
@@ -15,29 +15,32 @@ register = template.Library()
 
 @register.filter
 def list_directory(browse_dir):
-    
+
     directories = []
     files = []
     filenames = os.listdir(browse_dir)
-    
+
     for filename in filenames:
         path = os.path.join(browse_dir, filename)
         is_dir = os.path.isdir(path)
-        
+
         link_to = None
+        size = None
         if os.path.islink(path):
             link_to = os.readlink(path)
-        
+        else:
+            size = os.path.getsize(path)
+
         item = {
             'name': filename,
-            'size': os.path.getsize(path),
+            'size': size,
             'is_dir': is_dir,
             'link_to': link_to,
         }
-        
+
         if is_dir:
             directories.append(item)
         else:
             files.append(item)
-    
+
     return directories + files
